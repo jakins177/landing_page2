2cff27a1caa8bab74e8125d08f1c7461bf629fbc
Josh <owner@Joshs-MBP.fios-router.home>
2020-09-04 17:44:57 -0400

    first bit of work on traffic secrets page which may become a standardized template for future landing pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from "react-player"
+import Signup from './components/Signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1><mark>Need more traffic to your page?</mark> </h1>
+<h2>"In NEW FREE BOOK 
+You will Discover 20+ Virtually UNKNOWN Secrets
+To Get Tons Of Traffic To *Any* Website, Funnel or Landing Page..."</h2>
+   
+   
+    <div className="video-wrap">
+      <ReactPlayer className="video"
+        url='https://youtu.be/6ul1rYkMAQ8'/>
+  </div>
+
+    <Signup/>
+   
+   
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <div>
+            <div>
+            <br/>
+                <button>Get Your FREE Copy!</button>
+                
+            </div>
+            <div>
+                <br/>
+            <img src="https://trafficsecrets.com/hosted/images/0c/5409028308437590c05ba2fcfe0d78/Traffic-Secrets-1080x1080px-Ad-03.png"  width="50%" />
+            </div>
+            </div>
+
+        )
+    }
+}
